66dc4edfb8f2fea1928ff02564113e9051f36ee5
Girija Viswanathan <58586934+GirijaViswa@users.noreply.github.com>
2020-04-02 15:20:40 -0700

    final changes

--- app/controllers/work_post_controller.rb
+++ app/controllers/work_post_controller.rb
@@ -7,7 +7,7 @@ class WorkPostController < ApplicationController
         selected_work = user_work.select{|work| work.video_id === params[:video_id]}
         user_post = WorkPost.new(information:params[:information],image:params[:image],user_work_id:selected_work[0].id)
         if user_post.save
-            render json: {msg:'Post created Successfully',status:"ok",post:user_post}
+            render json: {msg:'Post created Successfully',status:"ok",post:user_post,posts:user.work_posts}
         else
             render json: {msg:"Unable to create the post. Try again later!"}
         end
